336d259c39110f0def0418593c4d4cb2fd6ae107
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:44:49 +0200

    Added front-page.php page template file

---
+++ front-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The front-page view composer file. This handles the data to
+ * show a nice front-page.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Mitch Hijlkema
+ */
+
+use Timber\Post;
+use Timber\Timber;
+use App\Helpers\Template;
+
+defined('ABSPATH') || exit;
+
+$context = Timber::get_context();
+$post = new Post();
+
+$context['post'] = $post;
+
+$templates = [
+	Template::viewTwigFile('front-page'),
+	Template::viewTwigFile('index'),
+];
+
+Timber::render(
+	apply_filters('wijnen/view-composer/front-page/templates', $templates),
+	apply_filters('wijnen/view-composer/front-page/context', $context)
+);
