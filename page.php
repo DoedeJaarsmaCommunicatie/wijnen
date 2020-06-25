25df98845e6f95bf52a23a700aea55b5e3124ef6
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:45:17 +0200

    Added page.php page template file

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This is for default pages.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Mitch Hijlkema
+ */
+
+use Timber\Post;
+use Timber\Timber;
+use App\Helpers\Template;
+
+defined('ABSPATH') || exit;
+
+$context = Timber::get_context();
+
+$context['post'] = new Post();
+
+$templates = [
+	Template::viewTwigFile('page'),
+];
+
+Timber::render(
+	apply_filters('wijnen/view-composer/page/templates', $templates),
+	apply_filters('wijnen/view-composer/page/context', $context)
+);
