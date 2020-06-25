3f1619fd812f1861008e059fc135d168ee662451
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 15:02:18 +0200

    [feature] Customizer basics

---
+++ src/Controllers/Customizer/CustomizerTypeManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Customizer;
+
+final class CustomizerTypeManager
+{
+    public const TEXT = 'text';
+    public const TEXTAREA = 'textarea';
+    public const EDITOR = 'editor';
+    public const RICH_TEXT = 'editor';
+}
