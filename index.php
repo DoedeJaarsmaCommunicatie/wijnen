5b884c991b0d09877b460523ee2045e38172d9d3
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:44:58 +0200

    Added index.php page template file

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * The template to use when no other template files are found
+ *
+ * Usage:
+ *  Add content to the $context array to use in the called template.
+ *  Add templates via the filter or in the templates array.
+ *
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @see https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @author Mitch Hijlkema
+ */
 
-$context = \Timber\Timber::get_context();
+use Timber\Post;
+use Timber\Timber;
+use App\Helpers\Template;
 
-return \Timber\Timber::render(
-    [
-        'views/page.html.twig',
-    ],
-    $context
+defined('ABSPATH') || exit;
+
+$context = Timber::get_context();
+
+$context['post'] = new Post();
+
+$templates = [
+	Template::viewTwigFile('index'),
+];
+
+Timber::render(
+	apply_filters('wijnen/view-composer/index/templates', $templates),
+	apply_filters('wijnen/view-composer/index/context', $context)
 );
