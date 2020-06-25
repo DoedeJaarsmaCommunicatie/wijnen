1fe9998c5cea6be9a2656d73864b7daa1e456158
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:44:37 +0200

    Added 404.php page template file

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This template is called when a 404 error is thrown.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @see https://codex.wordpress.org/Creating_an_Error_404_Page
+ * @author Mitch Hijlkema
+ */
+
+use Timber\Post;
+use App\Helpers\Template;
+use Timber\Timber;
+
+defined('ABSPATH') || exit;
+
+$context = Timber::get_context();
+$context['post'] = new Post();
+
+$templates = [
+	Template::viewHtmlTwigFile('404'),
+];
+
+Timber::render(
+	apply_filters('wijnen/view-composers/404/templates', $templates),
+	apply_filters('wijnen/view-composers/404/context', $context)
+);
