1b43aca4820b2f780271ea8b4831a0493fd2d89d
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:44:16 +0200

    Added wijnclub styles with different colors

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          default: 'hsl(0,48%,59%)',
+        },
+        secondary: {
+          default: 'hsl(177,40%,73%)',
+          def: 'hsl(173,58%,39%)',
+        },
+        black: {
+          default: 'hsl(0,0%,0%)',
+        },
+        white: {
+          default: 'hsl(0,0%,100%)',
+        },
+        grey: {
+          default: 'hsl(0,0%,58%)'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
