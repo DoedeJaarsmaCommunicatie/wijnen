1391f72f289cafcdea81f67bfe64a7e0b4aa2ba3
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-25 11:57:57 +0200

    Add commented code to easily enable .env files

--- src/app.php
+++ src/app.php
@@ -4,4 +4,8 @@ namespace App;
 use App\Bootstrap\Container;
 use App\Providers\AppServiceProvider;
 
+
+//To use the environment uncomment this.
+//Container::get(\App\Bootstrap\Env::class);
+
 Container::get(AppServiceProvider::class);
