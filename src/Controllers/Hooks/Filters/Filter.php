4651abee3a49f04c236e0cb9a3d9ad80d24c11b0
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-07-30 09:31:45 +0200

    [cleanup] added docblocks
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- src/Controllers/Hooks/Filters/Filter.php
+++ src/Controllers/Hooks/Filters/Filter.php
@@ -4,8 +4,21 @@ namespace App\Controllers\Hooks\Filters;
 
 abstract class Filter
 {
+
+    /**
+     * The filter function called on the hook described in hook function.
+     *
+     * @return mixed
+     * @see Filter::hook()
+     */
     abstract public function filter();
 
+    /**
+     * The hook where to attach the filter function on.
+     *
+     * @return string
+     * @see Filter::filter()
+     */
     abstract public function hook();
 
     public function priority(): int
