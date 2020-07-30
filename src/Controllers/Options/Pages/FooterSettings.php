2444b8a7aded9018207d84ec732d3f917dee8165
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-07-30 09:32:43 +0200

    [cleanup] [add] Support for mailchimp image
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- src/Controllers/Options/Pages/FooterSettings.php
+++ src/Controllers/Options/Pages/FooterSettings.php
@@ -10,24 +10,25 @@ use App\Controllers\Options\Traits\NoCustomParent;
 
 class FooterSettings implements Option
 {
-	use NoCustomParent;
-	use InAdminBar;
+    use NoCustomParent;
+    use InAdminBar;
 
-	public function register ()
-	{
-		return Container::make('theme_options', 'Footer')
-			->add_fields($this->fields());
+    public function register()
+    {
+        return Container::make('theme_options', 'Footer')
+            ->add_fields($this->fields());
+    }
 
-	}
+    protected function fields(): array
+    {
+        $fields = [];
+        $fields [] = Field::make('rich_text', 'footer_country_lists');
+        $fields [] = Field::make('rich_text', 'footer_domains_list');
+        $fields [] = Field::make('rich_text', 'footer_grapes_list');
+        $fields [] = Field::make('rich_text', 'footer_regions_list');
 
-	protected function fields():array
-	{
-		$fields = [];
-		$fields []= Field::make('rich_text', 'footer_country_lists');
-		$fields []= Field::make('rich_text', 'footer_domains_list');
-		$fields []= Field::make('rich_text', 'footer_grapes_list');
-		$fields []= Field::make('rich_text', 'footer_regions_list');
+        $fields [] = Field::make('image', 'mailchimp_form_image', 'Afbeelding Mailchimp Formulier');
 
-		return $fields;
-	}
+        return $fields;
+    }
 }
