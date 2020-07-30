71586419e745a2c7b8250b52e2fd58c1fdf3f53d
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-07-30 09:17:16 +0200

    Whitespacing
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- src/config/app.php
+++ src/config/app.php
@@ -1,20 +1,30 @@
 <?php
 namespace App;
 
-use App\Providers\{AjaxServiceProvider, CarbonServiceProvider, ContentServiceProvider, CustomizerServiceProvider, ElementorServiceProvider, MenuServiceProvider, FunctionServiceProvider, HookServiceProvider, AssetsServiceProvider, RestServiceProvider, UpdateServiceProvider};
+use App\Providers\AjaxServiceProvider;
+use App\Providers\MenuServiceProvider;
+use App\Providers\HookServiceProvider;
+use App\Providers\RestServiceProvider;
+use App\Providers\CarbonServiceProvider;
+use App\Providers\AssetsServiceProvider;
+use App\Providers\UpdateServiceProvider;
+use App\Providers\ContentServiceProvider;
+use App\Providers\FunctionServiceProvider;
+use App\Providers\ElementorServiceProvider;
+use App\Providers\CustomizerServiceProvider;
 
 return [
-	'providers'     => [
-	    MenuServiceProvider::class,
-		ContentServiceProvider::class,
-		FunctionServiceProvider::class,
-		HookServiceProvider::class,
-		AssetsServiceProvider::class,
-		AjaxServiceProvider::class,
-		CustomizerServiceProvider::class,
-		RestServiceProvider::class,
-		ElementorServiceProvider::class,
-		CarbonServiceProvider::class,
-		UpdateServiceProvider::class,
+    'providers'     => [
+        MenuServiceProvider::class,
+        ContentServiceProvider::class,
+        FunctionServiceProvider::class,
+        HookServiceProvider::class,
+        AssetsServiceProvider::class,
+        AjaxServiceProvider::class,
+        CustomizerServiceProvider::class,
+        RestServiceProvider::class,
+        ElementorServiceProvider::class,
+        CarbonServiceProvider::class,
+        UpdateServiceProvider::class,
     ]
 ];
